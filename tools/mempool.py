d10ead2d6a7ef479bba69c5a24c3c131bf334789
Romano <romanornr@gmail.com>
2019-03-15 18:44:30 +0100

    Viacoin: RPC ports and data dir

--- tools/mempool.py
+++ tools/mempool.py
@@ -51,9 +51,9 @@ def main():
     args = parser.parse_args()
 
     if args.testnet:
-        d = Daemon(port=18332, cookie_dir='~/.bitcoin/testnet3')
+        d = Daemon(port=25222, cookie_dir='~/.bitcoin/testnet3')
     else:
-        d = Daemon(port=8332, cookie_dir='~/.bitcoin')
+        d = Daemon(port=5222, cookie_dir='~/.bitcoin')
 
     txids, = d.request('getrawmempool', [[False]])
     txids = list(map(lambda a: [a], txids))
